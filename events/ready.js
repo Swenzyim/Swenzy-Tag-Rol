abd27f79b9034a62fa848cf47a901d2ee8a881a1
Swenzy <qnuran565@gmail.com>
2026-01-16 09:49:53 +0400

    Remove heart emoji from client import comment

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const client = require("../index"); // Swenzy ❤️ ExCODE
+const client = require("../index"); // Swenzy ❤️
 
 client.on("ready", () => {
     console.log(`${client.user.tag} ile giriş yapıldı!`)
